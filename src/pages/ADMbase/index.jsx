0f9db88be05ee28b8754ae46be7c59e5e106a271
Caio Coronel <caiocoronel10@gmail.com>
2024-11-08 11:53:13 -0300

    feature(PRACA): Adicionado rotas entre os botões de tela de visualização

--- src/pages/ADMbase/index.jsx
+++ src/pages/ADMbase/index.jsx
@@ -1,32 +1,30 @@
-import './ADMbase.css'
-import Logo from "../images/logo_full.png"
-import Diario from '../ADMdiario/index.jsx'
-import Semanal from '../ADMsemanal/index.jsx'
-import Mensal from '../ADMmensal/index.jsx'
+import React, { useState } from 'react';
+import './ADMbase.css';
+import Logo from "../images/logo_full.png";
+import Diario from '../ADMdiario/index.jsx';
+import Semanal from '../ADMsemanal/index.jsx';
+import Mensal from '../ADMmensal/index.jsx';
 
+export default function Adm() {
+    const [activeComponent, setActiveComponent] = useState('Mensal');
 
-export default function adm () {
     return (
-    
-<>
-<div className='bodyADM'>
-
-    <aside className="sidebar">
-        <div className="logo">
-            <img src={Logo} alt="Ícone" />
-        </div>
-        <div className="menu">
-            <button>Diário</button>
-            <button>Semanal</button>
-            <button>Mensal</button>
-        </div>
-    </aside>
-        {/* <Diario/> */}
-        {/* <Semanal/> */}
-        <Mensal/>
-   
-</div>   
-</>
-
-)
+        <>
+            <div className='bodyADM'>
+                <aside className="sidebar">
+                    <div className="logo">
+                        <img src={Logo} alt="Ícone" />
+                    </div>
+                    <div className="menu">
+                        <button onClick={() => setActiveComponent('Diario')}>Diário</button>
+                        <button onClick={() => setActiveComponent('Semanal')}>Semanal</button>
+                        <button onClick={() => setActiveComponent('Mensal')}>Mensal</button>
+                    </div>
+                </aside>
+                {activeComponent === 'Diario' && <Diario />}
+                {activeComponent === 'Semanal' && <Semanal />}
+                {activeComponent === 'Mensal' && <Mensal />}
+            </div>
+        </>
+    );
 }
